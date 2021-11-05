912142be71d007bfec8330c78bbcad589c066e51
GBFredrik <fredrik.gerdin.borjesson@hotmail.com>
2021-11-05 14:11:21 +0100

    Add Isda engine to engine factory

--- OREData/ored/portfolio/enginefactory.cpp
+++ OREData/ored/portfolio/enginefactory.cpp
@@ -208,6 +208,7 @@ void EngineFactory::addDefaultBuilders() {
     registerBuilder(boost::make_shared<data::LinearTsrDurationAdjustedCmsCouponPricerBuilder>());
 
     registerBuilder(boost::make_shared<MidPointCdsEngineBuilder>());
+    registerBuilder(boost::make_shared<IsdaCdsEngineBuilder>());
     registerBuilder(boost::make_shared<BlackCdsOptionEngineBuilder>());
     registerBuilder(boost::make_shared<CommodityForwardEngineBuilder>());
     registerBuilder(boost::make_shared<CommodityEuropeanOptionEngineBuilder>());
