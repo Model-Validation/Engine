27eb46aa08cd9d449e6fc47b99f98db7383bc46c
Fredrik Gerdin Börjesson <fredrik.gerdin.borjesson@hotmail.com>
2024-09-19 14:02:46 +0200

    Add building of interpolated FX point curves

--- OREData/ored/marketdata/yieldcurve.hpp
+++ OREData/ored/marketdata/yieldcurve.hpp
@@ -61,7 +61,7 @@ class ReferenceDataManager;
 class YieldCurve {
 public:
     //! Supported interpolation variables
-    enum class InterpolationVariable { Zero, Discount, Forward };
+    enum class InterpolationVariable { Zero, Discount, Forward, SwapPoints };
 
     //! Supported interpolation methods
     enum class InterpolationMethod {
@@ -155,6 +155,8 @@ private:
     void buildIborFallbackCurve();
     //! Build a yield curve that uses QuantExt::bondYieldShiftedCurve
     void buildBondYieldShiftedCurve();
+    //! Build an implied curve from a price curve of interpolated FX forwards
+    void buildInterpolatedFxForwardCurve();
 
     //! Return the yield curve with the given \p id from the requiredYieldCurves_ map
     QuantLib::ext::shared_ptr<YieldCurve> getYieldCurve(const std::string& ccy, const std::string& id) const;
